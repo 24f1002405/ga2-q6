84d23b333459d5a28147fb226cd224a7779bba81
Rajat <24f1002405@ds.study.iitm.ac.in>
2025-05-24 01:31:42 +0530

    e

--- api/index.js
+++ api/index.js
@@ -6,8 +6,8 @@ export async function GET(request) {
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const data = JSON.parse(fileContent);
     try {
-        const { name } = request.query;
-        return Response.json(name);
+        // console.log(request.url);
+        return Response.json(request.url);
     } catch (err) {
         return Response.json({"message": err.message})
     }
