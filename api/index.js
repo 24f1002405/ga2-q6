f409bd71a73b9a49d873b3aeae889b366b71f831
Rajat <24f1002405@ds.study.iitm.ac.in>
2025-05-24 01:38:58 +0530

    h

--- api/index.js
+++ api/index.js
@@ -5,6 +5,9 @@ export async function GET(request) {
     const filePath = path.join(process.cwd(), 'data.json');
     const fileContent = fs.readFileSync(filePath, 'utf-8');
     const data = JSON.parse(fileContent);
-    console.log(request);
-    return Response.json(request);
+    
+    const { searchParams } = new URL(request.url);
+    const names = searchParams.getAll('name');
+
+    return Response.json(names);
 }
